db7f07cc636acf5d41d084d85097378f1fef504b
Abd Ul-khalek Al-Fakih <abod532943@gmail.com>
2024-07-01 04:18:40 +0900

    Update optim.py

--- utils/optim.py
+++ utils/optim.py
@@ -5,31 +5,21 @@ def get_optimizer(args, net: nn.Module):
     lr, weight_decay = args.lr, args.weight_decay
 
     if args.optim == 'adamw':
-        optimizer = torch.optim.AdamW(net.parameters(), lr,amsgrad=True, weight_decay=weight_decay)
+        optimizer = torch.optim.AdamW(net.parameters(), lr, weight_decay=weight_decay)
     elif args.optim == 'sgd':
-        optimizer = torch.optim.SGD(net.parameters(), lr, momentum=args.beta1, nesterov=True)
+        optimizer = torch.optim.SGD(net.parameters(), lr, momentum=0.9, nesterov=False,dampening=0.)
     else:
         optimizer = torch.optim.Adam(net.parameters(), lr, weight_decay=weight_decay)     # default is adam
 
     return optimizer
-# def get_meta_optimizer(args, net: nn.Module):
-#     lr, weight_decay = args.lr, args.weight_decay
-#
-#     if args.meta_optim == 'adamw':
-#         meta_optimizer = torch.optim.AdamW(net.parameters(), lr, weight_decay=weight_decay)
-#     elif args.meta_optim == 'sgd':
-#         meta_optimizer = torch.optim.SGD(net.parameters(), lr, momentum=args.beta1, nesterov=True)
-#     else:
-#         meta_optimizer = torch.optim.Adam(net.parameters(), lr, weight_decay=weight_decay)     # default is adam
-#
-#     return meta_optimizer
+
 def get_clean_optimizer(args, net: nn.Module):
     lr, weight_decay = args.lr, args.weight_decay
 
     if args.clean_optim == 'adamw':
-        clean_optimizer = torch.optim.AdamW(net.parameters(), lr,amsgrad=True, weight_decay=weight_decay)
+        clean_optimizer = torch.optim.AdamW(net.parameters(), lr, weight_decay=weight_decay)
     elif args.clean_optim == 'sgd':
-        clean_optimizer = torch.optim.SGD(net.parameters(), lr, momentum=args.beta1, nesterov=True)
+        clean_optimizer = torch.optim.SGD(net.parameters(), lr, momentum=0.9, nesterov=True)
     else:
         clean_optimizer = torch.optim.Adam(net.parameters(), lr, weight_decay=weight_decay)     # default is adam
 
