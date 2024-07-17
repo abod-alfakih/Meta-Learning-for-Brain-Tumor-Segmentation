8cb0b0dd9cc8e9f79484e4e5a29aa26fe75f2f7d
Kanghyun <khryu@kist.re.kr>
2024-07-17 12:35:38 +0900

    commit

--- utils/train_utils.py
+++ utils/train_utils.py
@@ -38,8 +38,9 @@ def save_learning_rate_history(learning_rate_history, exp_dir):
     with open(lr_file_path, "a") as f:
         for lr in learning_rate_history:
             f.write(f"{lr}\n")
+
 def train(self, args, epoch, model, meta_net, train_loader, loss_fn, meta_loss_fn, optimizer, meta_optimizer, scheduler,
-          meta_scheduler, scaler,scaler_2, writer, logger):
+          meta_scheduler, writer, logger):
     data_time = AverageMeter('Data', ':6.3f')
     batch_time = AverageMeter('Time', ':6.3f')
     bce_meter = AverageMeter('BCE', ':.4f')
@@ -67,11 +68,6 @@ def train(self, args, epoch, model, meta_net, train_loader, loss_fn, meta_loss_f
                 pseudo_net = torch.nn.parallel.DistributedDataParallel.cuda()
             pseudo_net.load_state_dict(model.state_dict())
             pseudo_net.train()
-            #########################################
-            # Using torchopt
-            
-            
-            #######################################
             pseudo_outputs = pseudo_net(image)
             if isinstance(pseudo_outputs, list):
                 pseudo_outputs = pseudo_outputs[0]
@@ -284,102 +280,3 @@ def infer(args, epoch, model: nn.Module, loss_fn, infer_loader, writer, logger, 
             f.write(f"Epoch {epoch if epoch is not None else 'None'} - Average {mode} Loss: {avg_loss:.4f}\n")
 
         return infer_metrics
-
-learning_rate_history = []
-loss_history = []
-class TrainModel:
-
-
-    def main(self):
-        args = parse_seg_args()
-        logger, writer = initialization(args)
-
-
-        meta_net = MLP(hidden_size=100, num_layers=1).cuda()
-        if args.data_parallel:
-            meta_net = nn.DataParallel(meta_net).cuda()
-        # dataloaders
-        meta_optimizer = get_clean_optimizer(args, meta_net)
-        meta_scheduler = get_clean_scheduler(args, meta_optimizer)
-        meta_loss = clean_SoftDiceBCEWithLogitsLoss().cuda()
-
-        train_cases, val_cases, test_cases = load_cases_split(args.cases_split)
-        train_loader = brats2021.get_train_loader(args, train_cases)
-        val_loader   = brats2021.get_infer_loader(args, val_cases)
-        test_loader  = brats2021.get_infer_loader(args, test_cases) # what is the test loader?
-
-        train_clean_cases, val_clean_cases, test_clean_cases = load_cases_split(args.clean_cases_split)
-        self.meta_dataloader = brats2021.get_clean_train_loader(args, train_clean_cases)
-
-
-        self.meta_dataloader_iter = iter(self.meta_dataloader)        # model & stuff
-        model = get_unet(args).cuda()
-        if args.data_parallel:
-            model = nn.DataParallel(model).cuda()
-        optimizer = get_optimizer(args, model)
-        scheduler = get_scheduler(args, optimizer)
-        loss = SoftDiceBCEWithLogitsLoss().cuda()
-
-        if args.amp:
-            scaler = GradScaler()
-            logger.info("==> Using AMP (Auto Mixed Precision)")
-        else:
-            scaler = None
-
-
-        if args.amp:
-            scaler_2 = GradScaler()
-            logger.info("==> Using AMP (Auto Mixed Precision)")
-        else:
-            scaler_2 = None
-
-        # load model
-        if args.weight_path is not None:
-            logger.info("==> Loading pretrain model...")
-            assert args.weight_path.endswith(".pth")
-            model_state = torch.load(args.weight_path)['model']
-            model.load_state_dict(model_state)
-
-        # train & val
-        logger.info("==> Training starts...")
-        best_model = {}
-        val_leaderboard = LeaderboardBraTS()
-
-        for epoch in range(args.epochs):
-
-            train(self,args, epoch, model,meta_net, train_loader, loss,meta_loss, optimizer,meta_optimizer, scheduler,meta_scheduler, scaler, scaler_2,writer, logger)
-
-            if (epoch >= 0):
-                logger.info(f"==> Validation starts...")
-                # inference on validation set
-                val_metrics = infer(args, epoch, model,loss, val_loader, writer, logger, mode='val')
-    
-                # model selection
-                val_leaderboard.update(epoch, val_metrics)
-                best_model.update({epoch: deepcopy(model.state_dict())})
-                logger.info(f"==> Validation ends...")
-
-            torch.cuda.empty_cache()
-        # ouput final leaderboard and its rank
-        val_leaderboard.output(args.exp_dir)
-
-        # test
-        best_epoch = val_leaderboard.get_best_epoch()
-        best_model = best_model[best_epoch]
-        model.load_state_dict(best_model)
-
-        # save the best model on validation set
-        if args.save_model:
-            logger.info("==> Saving...")
-            state = {'model': best_model, 'epoch': best_epoch, 'args':args}
-            torch.save(state, os.path.join(
-                args.exp_dir, f"test_epoch_{best_epoch:02d}", f'best_ckpt.pth'))
-
-        logger.info("==> Testing starts...")            
-        infer(args, best_epoch, model, loss, test_loader, writer, logger, mode='test', save_pred=args.save_pred)
-        logger.info("==> Testing ends...")
-
-
-if __name__ == '__main__':
-    trainer = TrainModel()
-    trainer.main()
