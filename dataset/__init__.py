6d79a4082e70b66a089346c75629666d2e5151ff
Kanghyun <khryu@kist.re.kr>
2024-07-18 15:21:59 +0900

    cleaned

--- dataset/__init__.py
+++
@@ -1,8 +0,0 @@
-# # from .dataset_utils import nib_load, RobustZScoreNormalization
-# import nibabel as nib
-#
-# # Example path, replace this with one of the printed paths from your debugging
-# test_path = 'C:\\Users\\USER\\Master-Degree\\DATA\\BraTS2021\\archive\\BraTS2021_Training_Data\\BraTS2021_00000\\BraTS2021_00000_flair.nii.gz'
-#             C:\Users\USER\Master-Degree\DATA\BraTS2021\archive\BraTS2021_Training_Data\BraTS2021_01658\BraTS2021_01658_flair.nii.gz
-# img = nib.load(test_path)
-# print(img.shape)  # This should print the dimensions of the image if successful
